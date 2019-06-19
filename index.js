529a3aadb507bda1fc41fce6164be82d1c07ca2c
Joaco Esteban <dev.joaco.esteban@gmail.com>
2019-06-19 00:21:10 -0300

    Token Request with timeout

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var request = require("request");
+var token;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/random', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/random', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+
+
+var options = 
+{ 
+    method: 'POST',
+    url: 'https://accounts.spotify.com/api/token',
+    headers: 
+    { 
+        'Authorization': 'Basic  ODMzYmJmNDcxOGIzNDRiM2E5MTUxM2ViNmUwMGFmNzE6ODk0M2U4OGE5ODFmNGMzY2E3MTI4N2NjODM4ZTUzMmQ=' 
+    },
+    form: 
+    {
+        'grant_type': 'client_credentials'
+    } 
+};
+
+
+request(options, function (error, response, body) 
+{
+  if (error) throw new Error(error);
+  body = JSON.parse(body)
+  token = body.access_token;
+  console.log(token);
+});
+
+
+setInterval(() => {
+  
+  request(options, function (error, response, body) 
+  {
+    if (error) throw new Error(error);
+    body = JSON.parse(body)
+    token = body.access_token;
+    console.log(token);
+  });
+}, 3600*1000);

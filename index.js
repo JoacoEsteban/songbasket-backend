faff1c538c03556e51c71ac3eace304f85723c6e
Joaco Esteban <dev.joaco.esteban@gmail.com>
2019-07-07 21:58:16 -0300

    -

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var request = require("request");
 const app = express();
 var SpotifyWebApi = require('spotify-web-api-node');
 
+const SPOTIFY_LOGIN_URL = 'https://accounts.spotify.com/authorize?client_id=30e3ebd25fd04ac5b1e2dfe889fdc90c&response_type=code&redirect_uri=https://spotify-api-wrapper-joaco.herokuapp.com/&scope=user-read-private user-read-email playlist-read-private';
+
+
 var token;
 const refresh_token = 'AQDF5jWtMdUAFTImnb1v1pZprK_aokOfgpdrOoCiZzV6kcUDcqNDQX5Z3nWNHvdeLgMhsxzIlOe_u4RhJpyXbNLC5d0b2xNc6JbGym8DUgEGwM4xMPTxLEphtjAn0Rl8ACwvEQ';
 var access_token = 'BQDkuo9jM02xXHLEgZXyeFgRGDWwzWWwQhwYp05rJQiLALdeRCAYpbndXYfG6UlQ3ljIwFVeCF7nYUxELpjERaQivx6Re0Iwh1e3NWi6fnmi-5jLTAyJbUJ4fOq2nc26uvTLr71U9IvPMD5eXMnab2O5RXTqb9-t0Gye4BIUQCV8W2Tm7fE';
@@ -40,7 +43,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-app.get('/', (req, res) => res.redirect('301', 'pages/index'))
+app.get('/', (req, res) => res.redirect('301', SPOTIFY_LOGIN_URL))
 
 var nameGLOB;
 var object;

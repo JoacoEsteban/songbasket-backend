7c2d2c946afa9b6d3b1083075955ed83f1fee060
Joaco Esteban <dev.joaco.esteban@gmail.com>
2019-07-08 01:59:58 -0300

    User auth and then getting user profile working

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ var request = require("request");
 const app = express();
 var SpotifyWebApi = require('spotify-web-api-node');
 
-const SPOTIFY_LOGIN_URL = 'https://accounts.spotify.com/authorize?client_id=30e3ebd25fd04ac5b1e2dfe889fdc90c&response_type=code&redirect_uri=https://spotify-api-wrapper-joaco.herokuapp.com/&scope=user-read-private user-read-email playlist-read-private';
+var SPOTIFY_LOGIN_URL = 'https://accounts.spotify.com/authorize?client_id=30e3ebd25fd04ac5b1e2dfe889fdc90c&response_type=code&redirect_uri=http://localhost:5000/handle_authorization/&scope=user-read-private+user-read-email+playlist-read-private';
 
 
 var token;
-const refresh_token = 'AQDF5jWtMdUAFTImnb1v1pZprK_aokOfgpdrOoCiZzV6kcUDcqNDQX5Z3nWNHvdeLgMhsxzIlOe_u4RhJpyXbNLC5d0b2xNc6JbGym8DUgEGwM4xMPTxLEphtjAn0Rl8ACwvEQ';
+var refresh_token = 'AQDF5jWtMdUAFTImnb1v1pZprK_aokOfgpdrOoCiZzV6kcUDcqNDQX5Z3nWNHvdeLgMhsxzIlOe_u4RhJpyXbNLC5d0b2xNc6JbGym8DUgEGwM4xMPTxLEphtjAn0Rl8ACwvEQ';
 var access_token = 'BQDkuo9jM02xXHLEgZXyeFgRGDWwzWWwQhwYp05rJQiLALdeRCAYpbndXYfG6UlQ3ljIwFVeCF7nYUxELpjERaQivx6Re0Iwh1e3NWi6fnmi-5jLTAyJbUJ4fOq2nc26uvTLr71U9IvPMD5eXMnab2O5RXTqb9-t0Gye4BIUQCV8W2Tm7fE';
+
+const CLIENT_ID = '30e3ebd25fd04ac5b1e2dfe889fdc90c';
+const CLIENT_SECRET = 'd01ade80ab7849ab999cb012654991df';
 
 var spotifyApi = new SpotifyWebApi({
   clientId: '30e3ebd25fd04ac5b1e2dfe889fdc90c',
@@ -43,7 +46,84 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-app.get('/', (req, res) => res.redirect('301', SPOTIFY_LOGIN_URL))
+//Redirect to Login URL
+app.get('/init', (req, res) => res.redirect(301, SPOTIFY_LOGIN_URL));
+
+
+//Arrives with Authorization Code
+app.get('/handle_authorization', (req, res) => {
+  var user = {};
+  var authorizationCode = req.query.code;
+  console.log('Code: ', authorizationCode);
+
+  if(authorizationCode !== undefined)
+  {
+    //Get Access Token and Refresh Token
+    var options = 
+    { 
+      method: 'POST',
+      url: 'https://accounts.spotify.com/api/token',
+      
+      form: 
+      { 
+        grant_type: 'authorization_code',
+        code: authorizationCode,
+        redirect_uri: 'http://localhost:5000/handle_authorization/',
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+      } 
+    };
+
+    request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    body = JSON.parse(body);
+    console.log(body);
+
+    access_token = body.access_token;
+    refresh_token = body.refresh_token;
+    
+    user.access_token = access_token;
+    user.refresh_token = refresh_token;
+    
+    //Gets User data
+    request({
+      url: 'https://api.spotify.com/v1/me',
+      headers:{Authorization: `Bearer ${user.access_token}`},
+    }, (error, response, body) =>{
+      if(error) throw new Error(error);
+      user.profile = JSON.parse(body);
+      console.log(user.profile);
+      
+      res.json(user.profile);
+    })
+    
+    });
+  }
+
+
+  // res.render('pages/success')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => res.render('pages/success'));
 
 var nameGLOB;
 var object;

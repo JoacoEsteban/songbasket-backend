84012a38ad94e5250083386dfa0947f54940047f
Joaco Esteban <dev.joaco.esteban@gmail.com>
2019-09-11 23:35:20 -0300

    mini fix

--- src/index.js
+++ src/index.js
@@ -247,7 +247,8 @@ function retrieveRedirect(res, data) {
 						res.json({ playlist: {same_version: true}, request: data })
 						return
 					}
-					SBFETCH.GetPlaylistTracks(data.playlist_id, [], 0, CurrentWrapper.giveMe.access_token())
+				}
+				SBFETCH.GetPlaylistTracks(data.playlist_id, [], 0, CurrentWrapper.giveMe.access_token())
 					.then(tracks => {
 						console.log('Number of tracks Retireved: ', tracks.length)
 						playlist.tracks.items = tracks
@@ -258,12 +259,11 @@ function retrieveRedirect(res, data) {
 						playlist.tracks.items = null
 						res.json({playlist, request: data})
 					})
-				})
-				.catch( error => {
-					console.log('Error when retrieving PL Data from GetPlaylistData: ', error)
-					res.json({playlist: null, request: data})
-				})
-			}
+			})
+			.catch( error => {
+				console.log('Error when retrieving PL Data from GetPlaylistData: ', error)
+				res.json({playlist: null, request: data})
+			})
 		break
 
 		//TODO

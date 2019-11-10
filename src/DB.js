3541d6fb0099e890dd894c4dd0c50e9ec081e0fb
Joaco Esteban <dev.joaco.esteban@gmail.com>
2019-11-09 23:59:49 -0300

    Saving conversions to DB

--- src/DB.js
+++ src/DB.js
@@ -1,105 +1,95 @@
-const uuid = require('uuid/v4')
+// Setting up the database connection
+const knex = require('knex')({
+  client: 'postgres',
+  connection: {
+    host: process.env.DATABASE_URL,
+    user: 'postgres',
+    password: process.env.DATABASE_PASSWORD,
+    database: 'songbasket_db',
+    charset: 'utf8'
+  }
+})
+const db = require('bookshelf')(knex)
 
-var DATA_BASE = new Array()
+// Defining models
+const YoutubeTracks = db.model('YoutubeTracks', {
+  tableName: 'youtube_tracks'
+})
+const SpotifyTracks = db.model('SpotifyTracks', {
+  tableName: 'spotify_tracks'
+})
+const Relations = db.model('Relations', {
+  tableName: 'conversion_relations'
+})
 
-// ::::::::::::::USER OBJECT::::::::::::::
-// {
-//     user_id
-//     access_token
-//     refresh_token
-//     SBID
-//     token_created_at
-// }
-
-function isNullOrUndefined(x) {return x === null || x === undefined}
-
-
-function getUserIndexFromSBID(SBID)
-{
-	for(let i = 0; i < DATA_BASE.length; i++)
-	{
-		if(DATA_BASE[i].SBID === SBID)
-		{
-			return i
-		}
-	}
-	return -1 //Not Found
-}
-function getUserIndexFromUserID(user_id)
-{
-	for(let i = 0; i < DATA_BASE.length; i++)
-	{
-		if(DATA_BASE[i].user_id === user_id)
-		{
-			return i
-		}
-	}
-	return -1 //Not Found
+const yt = {
+  getById(youtube_id) {
+    return YoutubeTracks.query({ where: { youtube_id } }).fetch()
+  },
+  addReg(youtube_id, snippet, duration) {
+    return YoutubeTracks.forge({ youtube_id, snippet: JSON.stringify(snippet), duration }).save()
+  }
 }
 
-exports.DB = {
+const sp = {
+  getById(spotify_id) {
+    return new Promise((resolve, reject) => {
+      SpotifyTracks.query({ where: { spotify_id } })
+        .fetch()
+        .then(res => resolve(res))
+        .catch(err => {
+          if (err.message === 'EmptyResponse') resolve(false)
+          reject(err)
+        })
+    })
+  },
+  addReg(spotify_id, bestMatch) {
+    return SpotifyTracks.forge({ spotify_id, best_match: bestMatch }).save()
+  }
+}
 
-	publish(newUser) //Recieves an Object with the User ID and Refresh and Access Tokens. SBID Is Created here for new users and returned
-	{
-		var userIndex = getUserIndexFromUserID(newUser.user_id)
+const rel = {
+  async getAllFrom(spotify_id) {
+    let trackExists = await sp.getById(spotify_id)
+    if (!trackExists) {
+      // Track not found
+      return false
+    }
+    return Relations.query({ where: { spotify_id } })
+    // TODO fetch from Tracks table
+  },
+  addReg(spotify_id, youtube_id) {
+    return Relations.forge({ spotify_id, youtube_id }).save()
+  },
+  insertAllFrom(spotify_id, results, bestMatch) {
+    let ammount = results.length
+    return new Promise((resolve, reject) => {
+      results.forEach(({ id, snippet, duration }) => {
+        yt.addReg(id, snippet, duration)
+          .then(() => {
+            if (id === bestMatch) {
+              sp.addReg(spotify_id, bestMatch)
+                .then(() => {
+                  this.addReg(spotify_id, id)
+                    .then(() => {
+                      if (--ammount === 0) resolve()
+                    })
+                    .catch(err => reject(err))
+                })
+            } else {
+              this.addReg(spotify_id, id)
+                .then(() => {
+                  if (--ammount === 0) resolve()
+                })
+                .catch(err => reject(err))
+            }
+          })
+          .catch(err => reject(err))
+      })
+    })
+  }
+}
 
-		if(userIndex === -1) //If user isn't in the DB create a SBID and Store it
-		{
-			newUser.SBID = uuid()
-            
-			DATA_BASE.push(newUser)
-			console.log('DATABASE::::::::::::::::', DATA_BASE[DATA_BASE.length - 1])
-
-			return newUser.SBID
-		}else
-		{ //Overwrites with new data
-			newUser.SBID = isNullOrUndefined(DATA_BASE[userIndex].SBID) ? uuid() : DATA_BASE[userIndex].SBID 
-
-			DATA_BASE[userIndex] = newUser
-			console.log('USER EXISTS, OVERWRITTEN WITH NEW DATA::::::::::::::::', DATA_BASE[userIndex])
-            
-			return newUser.SBID
-		}
-	},
-
-
-
-
-
-
-
-
-    
-	//Get User
-	getUserFromUserID(user_id)
-	{
-		index = getUserIndexFromUserID(user_id)
-		return index === -1 ? null : DATA_BASE[index]
-	},
-
-	getUserFromSBID(SBID)
-	{
-		index = getUserIndexFromSBID(SBID)
-		return index === -1 ? null : DATA_BASE[index]
-	},
-
-
-
-
-
-
-
-
-
-
-	updateToken(SBID, token)
-	{
-		var userIndex = getUserIndexFromSBID(SBID)
-        
-		if(userIndex === -1 )
-		{
-			console.error('ERROR WHILE UPDATING USER TOKEN: USER NOT FOUND')
-		}else DATA_BASE[userIndex].access_token = token //Updates token from database
-	}
-
+module.exports = {
+  DB: rel
 }
--- src/index.js
+++ src/index.js
@@ -17,19 +17,19 @@ const { SBFETCH } = require('./SBFETCH')
 const { logme } = require('./logme')
 
 let Wrapper = new SpotifyAPI({
-	client_id: CLIENT_ID,
-	client_secret: CLIENT_SECRET,
-	redirect_uri: REDIRECT_URI
+  client_id: CLIENT_ID,
+  client_secret: CLIENT_SECRET,
+  redirect_uri: REDIRECT_URI
 })
 
 let GuestWrapper = new SpotifyAPI({
-	client_id: CLIENT_ID,
-	client_secret: CLIENT_SECRET,
-	logged: false
+  client_id: CLIENT_ID,
+  client_secret: CLIENT_SECRET,
+  logged: false
 })
 
 let YouTubeWrapper = new YouTubeAPI({
-	access_tokens: YOUTUBE_API_KEYS
+  access_tokens: YOUTUBE_API_KEYS
 })
 
 // Gets Client Credentials Token and sets a timeout
@@ -37,8 +37,8 @@ GuestWrapper.CCInit()
 
 
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*')
-	next()
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -52,250 +52,260 @@ app.get('/init', (req, res) => res.redirect(301, SPOTIFY_LOGIN_URL))
 
 //Arrives with Authorization Code
 app.get('/handle_authorization', (req, res) => {
-	var newUser = {} //will store newUser data and then publish it to DB
-	var authorizationCode = req.query.code
-	logme(`Authorization Code: ${authorizationCode}`)
+  var newUser = {} //will store newUser data and then publish it to DB
+  var authorizationCode = req.query.code
+  logme(`Authorization Code: ${authorizationCode}`)
 
-	if (authorizationCode !== undefined) {
-		//Get Access Token and Refresh Token
-		Wrapper.authorizationCodeGrant(authorizationCode)
-			.then(function ({ access_token, refresh_token }) {
+  if (authorizationCode !== undefined) {
+    //Get Access Token and Refresh Token
+    Wrapper.authorizationCodeGrant(authorizationCode)
+      .then(function ({ access_token, refresh_token }) {
 
-				//Updates user to be pushed to database
-				newUser.access_token = access_token
-				newUser.refresh_token = refresh_token
-				newUser.token_created_at = Date.now()
+        //Updates user to be pushed to database
+        newUser.access_token = access_token
+        newUser.refresh_token = refresh_token
+        newUser.token_created_at = Date.now()
 
-				// Set the access token on the API object to use it in later calls
-				Wrapper.setAccessToken(newUser.access_token)
-				Wrapper.setRefreshToken(newUser.refresh_token)
-			},
-			function (err) {
-				console.log('SOMETHING WENT WRONG! When retrieving User Access Tokens', err)
-				res.redirect(301, `${BACKEND}/fail`)
-			})
-			.then(() => {
-				//Gets New User's ID to push to DB 
-				Wrapper.getMe()
-					.then(function (newUserData) {
+        // Set the access token on the API object to use it in later calls
+        Wrapper.setAccessToken(newUser.access_token)
+        Wrapper.setRefreshToken(newUser.refresh_token)
+      },
+        function (err) {
+          console.log('SOMETHING WENT WRONG! When retrieving User Access Tokens', err)
+          res.redirect(301, `${BACKEND}/fail`)
+        })
+      .then(() => {
+        //Gets New User's ID to push to DB 
+        Wrapper.getMe()
+          .then(function (newUserData) {
 
-						console.log(newUserData)
-						newUser.user_id = newUserData.id
+            console.log(newUserData)
+            newUser.user_id = newUserData.id
 
-						newUser.SBID = DB.publish(newUser) //Update database with New User. The SBID Is Returned from inside the function
+            newUser.SBID = DB.publish(newUser) //Update database with New User. The SBID Is Returned from inside the function
 
-						res.set({
-							user_id: newUser.user_id,
-							SBID: newUser.SBID,
-							success: true,
-						})
+            res.set({
+              user_id: newUser.user_id,
+              SBID: newUser.SBID,
+              success: true,
+            })
 
-						res.send()
-					})
+            res.send()
+          })
 
-			})
+      })
 
-	} else {
-		res.redirect(301, `${BACKEND}/fail`)
-	}
+  } else {
+    res.redirect(301, `${BACKEND}/fail`)
+  }
 })
 
 
 app.get('/guest_sign_in', (req, res) => {
-	let {user_id} = req.query
-	if(user_id === 'null' || user_id === 'undefined' || user_id === null || user_id === undefined){
-		res.status(400)
-		res.set({
-			reason: 'username not provided'
-		})
-		res.send()
-	}else{
-		GuestWrapper.setUserId(user_id)
-		GuestWrapper.giveMe.user()
-			.then(resp => {
-				console.log('user:::::', resp)
-				resp.code = 200
-				res.json(resp)
-			})
-			.catch(err => {
-				res.json(err)
-				res.send()
-			})
-	}
+  let { user_id } = req.query
+  if (user_id === 'null' || user_id === 'undefined' || user_id === null || user_id === undefined) {
+    res.status(400)
+    res.set({
+      reason: 'username not provided'
+    })
+    res.send()
+  } else {
+    GuestWrapper.setUserId(user_id)
+    GuestWrapper.giveMe.user()
+      .then(resp => {
+        console.log('user:::::', resp)
+        resp.code = 200
+        res.json(resp)
+      })
+      .catch(err => {
+        res.json(err)
+        res.send()
+      })
+  }
 
 })
 
 
 app.get('/fail', (req, res) => {
-	res.render('pages/access_denied', { url: SPOTIFY_LOGIN_URL })
+  res.render('pages/access_denied', { url: SPOTIFY_LOGIN_URL })
 })
 
 // Post due to query weight limits
 app.post('/youtubize', (req, res) => {
-	let track = JSON.parse(req.body.track)
-	console.log(track)
-	YouTubeWrapper.Youtubize(track)
-		.then(conclusion => {
-			console.log('Terminamo', conclusion)
-			res.json(conclusion)
-		})
-		.catch(err => console.log(err))
+  let track = JSON.parse(req.body.track)
+  console.log(track)
+  DB.getAllFrom(track.id)
+    .then(conversion => {
+      if (!conversion) {
+        console.log('Track not cached, retrieving')
+        YouTubeWrapper.Youtubize(track)
+          .then(conclusion => {
+            console.log('Track Retrieved', conclusion)
+            DB.insertAllFrom(conclusion.id, conclusion.yt, conclusion.bestMatch)
+              .then(() => {
+                console.log('Track Cached')
+              })
+            res.json(conclusion)
+          })
+          .catch(err => console.error(err))
+      }
+    })
 })
 
 app.get('/yt_details', (req, res) => {
-	let {ytId} = req.query
-	console.log('getting youtube details from ', ytId)
-	let result = (/(https:\/\/www.youtube.com.watch\?v=)?([a-zA-Z0-9-_]{11})/).exec(ytId)
-	if (result === null) {
-		let reason = 'Invalid YouTube Url or ID'
-		console.error(reason)
-		res.status(400)
-		return res.json({error: true, reason})
-	}
+  let { ytId } = req.query
+  console.log('getting youtube details from ', ytId)
+  let result = (/(https:\/\/www.youtube.com.watch\?v=)?([a-zA-Z0-9-_]{11})/).exec(ytId)
+  if (result === null) {
+    let reason = 'Invalid YouTube Url or ID'
+    console.error(reason)
+    res.status(400)
+    return res.json({ error: true, reason })
+  }
 
-	YouTubeWrapper.getDetails(result[2])
-		.then(response => {
-			console.log('details Retrieved')
-			res.send(response)
-		})
-		.catch(err => {
-			res.status(400)
-			res.json({error: true, reason: err})
-		})
+  YouTubeWrapper.getDetails(result[2])
+    .then(response => {
+      console.log('details Retrieved')
+      res.send(response)
+    })
+    .catch(err => {
+      res.status(400)
+      res.json({ error: true, reason: err })
+    })
 })
 
 app.get('/retrieve', (req, res) => {
-	let query = req.query
-	let requestParams = {
-		retrieve: query.retrieve,
-		logged: query.logged.trim() == 'true' ? true : false, //wheter it's a SB logged user
-	}
-	if(query.SBID) {
-		requestParams.SBID = query.SBID.trim() === 'null' ? null : query.SBID.trim() //SB User ID
-	}
+  let query = req.query
+  let requestParams = {
+    retrieve: query.retrieve,
+    logged: query.logged.trim() == 'true' ? true : false, //wheter it's a SB logged user
+  }
+  if (query.SBID) {
+    requestParams.SBID = query.SBID.trim() === 'null' ? null : query.SBID.trim() //SB User ID
+  }
 
-	//Request Validation
-	var isValid = true
-	var reason = ''
-	if (requestParams.logged === true && requestParams.SBID === null) {
-		isValid = false
-		reason += 'SongBasket ID (SBID) missing.'
-	}
+  //Request Validation
+  var isValid = true
+  var reason = ''
+  if (requestParams.logged === true && requestParams.SBID === null) {
+    isValid = false
+    reason += 'SongBasket ID (SBID) missing.'
+  }
 
-	switch (requestParams.retrieve) {
-	case 'playlist_tracks':
-		//in case of retrieving playlist tracks:
-		requestParams = {
-			...requestParams,
-			playlist_id: query.playlist_id === undefined || query.playlist_id.trim() === 'null' ?  null : query.playlist_id.trim(),
-			snapshot_id: query.snapshot_id === undefined || query.snapshot_id.trim() === 'null' ?  null : query.snapshot_id.trim()
-		}
-		
-		if (requestParams.playlist_id === null) {
-			isValid = false
-			reason += 'playlist_id ID missing. '
-		}
+  switch (requestParams.retrieve) {
+    case 'playlist_tracks':
+      //in case of retrieving playlist tracks:
+      requestParams = {
+        ...requestParams,
+        playlist_id: query.playlist_id === undefined || query.playlist_id.trim() === 'null' ? null : query.playlist_id.trim(),
+        snapshot_id: query.snapshot_id === undefined || query.snapshot_id.trim() === 'null' ? null : query.snapshot_id.trim()
+      }
 
-		break
-		
-	case 'playlists':
-		requestParams = {
-			...requestParams,
-			user_id: query.user_id.trim() === '' ? false : query.user_id.trim(),
-			offset: parseInt(query.offset.trim()),
-			retrieve_user_data: query.retrieve.trim() === 'true' ? true : query.retrieve.trim() === 'false' ? false : 'invalid',
-		}
+      if (requestParams.playlist_id === null) {
+        isValid = false
+        reason += 'playlist_id ID missing. '
+      }
 
-		if (requestParams.user_id === false) {
-			isValid = false
-			reason += 'User ID missing. '
-		}
-		
-		break
-	}
+      break
 
-	console.log('REQUEST PARAMS:::::', requestParams) 
+    case 'playlists':
+      requestParams = {
+        ...requestParams,
+        user_id: query.user_id.trim() === '' ? false : query.user_id.trim(),
+        offset: parseInt(query.offset.trim()),
+        retrieve_user_data: query.retrieve.trim() === 'true' ? true : query.retrieve.trim() === 'false' ? false : 'invalid',
+      }
 
-	if (!isValid) {
-		res.status(400)
-		res.set({
-			reason
-		})
-		res.send()
-		console.log('Bad request, reason:', reason)
-		return
-	}
+      if (requestParams.user_id === false) {
+        isValid = false
+        reason += 'User ID missing. '
+      }
 
-	if (!requestParams.logged) GuestWrapper.setUserId(requestParams.user_id)
-	else {
-		// TODO Handle logged user
-	}
-	let data = requestParams
-	retrieveRedirect(res, data)
+      break
+  }
+
+  console.log('REQUEST PARAMS:::::', requestParams)
+
+  if (!isValid) {
+    res.status(400)
+    res.set({
+      reason
+    })
+    res.send()
+    console.log('Bad request, reason:', reason)
+    return
+  }
+
+  if (!requestParams.logged) GuestWrapper.setUserId(requestParams.user_id)
+  else {
+    // TODO Handle logged user
+  }
+  let data = requestParams
+  retrieveRedirect(res, data)
 })
 
 
 //TOKEN Used only on guests, else using API Wrapper
-function retrieveRedirect(res, data) {			
-	let CurrentWrapper	
+function retrieveRedirect(res, data) {
+  let CurrentWrapper
 
-	if (data.logged && data.logged !== 'invalid') { CurrentWrapper = Wrapper }
-	else { CurrentWrapper = GuestWrapper }
+  if (data.logged && data.logged !== 'invalid') { CurrentWrapper = Wrapper }
+  else { CurrentWrapper = GuestWrapper }
 
-	switch (data.retrieve) {
-	case 'playlists':
-		SBFETCH.GetUserPlaylists(data, CurrentWrapper.giveMe.access_token())
-			.then(playlists => {
-				if (data.retrieve_user_data) {
-					GuestWrapper.setUserId(data.user_id)
-					CurrentWrapper.giveMe.user()
-						.then(user => {
-							res.json({ playlists, user, request: data, code: 200 })
-						}, error => {
-							console.log(error)
-							res.json(error)
-						})
-				} else res.json({ playlists, request: data, code: 200 })
-			}
-			)
+  switch (data.retrieve) {
+    case 'playlists':
+      SBFETCH.GetUserPlaylists(data, CurrentWrapper.giveMe.access_token())
+        .then(playlists => {
+          if (data.retrieve_user_data) {
+            GuestWrapper.setUserId(data.user_id)
+            CurrentWrapper.giveMe.user()
+              .then(user => {
+                res.json({ playlists, user, request: data, code: 200 })
+              }, error => {
+                console.log(error)
+                res.json(error)
+              })
+          } else res.json({ playlists, request: data, code: 200 })
+        }
+        )
 
-		break
+      break
 
-	case 'playlist_tracks':
-		SBFETCH.GetPlaylistData(data.playlist_id, CurrentWrapper.giveMe.access_token())
-			.then(playlist => {
-				if (data.snapshot_id) {
-					if (data.snapshot_id === playlist.snapshot_id) {
-						console.log('SAME VERSION')
-						res.json({ playlist: {same_version: true}, request: data })
-						return
-					}
-				}
-				SBFETCH.GetPlaylistTracks(data.playlist_id, [], 0, CurrentWrapper.giveMe.access_token())
-					.then(tracks => {
-						console.log('Number of tracks Retireved: ', tracks.length)
-						playlist.tracks.items = tracks
-						res.json({ playlist, request: data })
-					})
-					.catch( error => {
-						console.log('Error when retrieving PL Tracks from GetPlaylistTracks: ', error)
-						playlist.tracks.items = null
-						res.json({playlist, request: data})
-					})
-			})
-			.catch( error => {
-				console.log('Error when retrieving PL Data from GetPlaylistData: ', error)
-				res.status(error.status)
-				res.send()
-				// res.json({playlist: null, error, request: data})
-			})
-		break
+    case 'playlist_tracks':
+      SBFETCH.GetPlaylistData(data.playlist_id, CurrentWrapper.giveMe.access_token())
+        .then(playlist => {
+          if (data.snapshot_id) {
+            if (data.snapshot_id === playlist.snapshot_id) {
+              console.log('SAME VERSION')
+              res.json({ playlist: { same_version: true }, request: data })
+              return
+            }
+          }
+          SBFETCH.GetPlaylistTracks(data.playlist_id, [], 0, CurrentWrapper.giveMe.access_token())
+            .then(tracks => {
+              console.log('Number of tracks Retireved: ', tracks.length)
+              playlist.tracks.items = tracks
+              res.json({ playlist, request: data })
+            })
+            .catch(error => {
+              console.log('Error when retrieving PL Tracks from GetPlaylistTracks: ', error)
+              playlist.tracks.items = null
+              res.json({ playlist, request: data })
+            })
+        })
+        .catch(error => {
+          console.log('Error when retrieving PL Data from GetPlaylistData: ', error)
+          res.status(error.status)
+          res.send()
+          // res.json({playlist: null, error, request: data})
+        })
+      break
 
-		//TODO
-	case 'user_profile':
-		// plMakeRequestTracks({ playlist_id: requestParams.playlist_id, token, callback: (playlist_id, tracks) => res.json({ playlist_id, tracks }) })
-		break
-	}
+    //TODO
+    case 'user_profile':
+      // plMakeRequestTracks({ playlist_id: requestParams.playlist_id, token, callback: (playlist_id, tracks) => res.json({ playlist_id, tracks }) })
+      break
+  }
 }
 
 

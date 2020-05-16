5e41a2dacab6d52ae242e739e6b5689c76f97e35
Joaco Esteban <dev.joaco.esteban@gmail.com>
2020-05-16 14:49:13 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.listen(process.env.PORT || 5000, () => console.log(`Listening on ${PORT}`))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 require('./routes/index.routes')(app)
 

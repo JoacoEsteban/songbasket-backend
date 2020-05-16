3339ca50f150d9602a4e9be7f64253e0de2b4743
Joaco Esteban <dev.joaco.esteban@gmail.com>
2020-05-16 14:28:13 -0300

    tryfix

--- src/env.config.js
+++ src/env.config.js
@@ -3,4 +3,4 @@ global.CONSTANTS = require('dotenv-flow').config().parsed
 global.CONSTANTS.PRODUCTION = utils.parseBool(process.env.PRODUCTION)
 global.CONSTANTS.PRODUCTION_DB = utils.parseBool(process.env.PRODUCTION_DB)
 
-console.log(global.CONSTANTS)
+console.log('test', require('dotenv-flow').config(), '\n\n')

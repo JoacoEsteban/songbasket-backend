e646a7ff8bb37c423d12b10a5a2fecbe7feb51f4
Joaco Esteban <dev.joaco.esteban@gmail.com>
2020-05-16 14:36:27 -0300

    tryfix

--- src/env.config.js
+++ src/env.config.js
@@ -1,5 +1,15 @@
 const utils = require('./utils')
-global.CONSTANTS = require('dotenv-flow').config().parsed
+global.CONSTANTS = process.env.PRODUCTION ? (() => {
+  return {
+    YOUTUBE_API_KEYS: process.env.YOUTUBE_API_KEYS,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_PORT: process.env.DATABASE_PORT,
+    PRODUCTION: true,
+    PRODUCTION_DB: true
+  }
+})() : require('dotenv-flow').config().parsed
 global.CONSTANTS.PRODUCTION = utils.parseBool(process.env.PRODUCTION)
 global.CONSTANTS.PRODUCTION_DB = utils.parseBool(process.env.PRODUCTION_DB)
 
